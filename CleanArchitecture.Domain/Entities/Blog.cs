3315acd5218140524b3f332e24a7141e7dfd90d6
Sundaram <sundaram@bookmycargo.net>
2024-11-22 02:08:00 +0530

    Initial commit

---
+++ CleanArchitecture.Domain/Entities/Blog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain.Entities
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int Author { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
